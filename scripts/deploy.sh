32cfe9f1de6a0d946bace581ce5d8a9a7bb5560f
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-12-12 09:09:14 -0500

    chore(release): patch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env sh
 
-echo 'Deployment Started ...'
-npm version $1
-
 echo 'Adding GIT Commit ...'
 git add -A
 git commit -m "chore(release): $1"
 git push
+
+echo 'Deployment Started ...'
+npm version $1
 
 echo 'Publishing NPM Package ...'
 npm publish
