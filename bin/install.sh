326014ddcd3fead5fa001caa82ae9963e912c1d6
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-12-12 09:19:47 -0500

    chore(release): patch

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,11 @@
 
 echo 'Beginning design system install ...'
 
+rm -rf package.json package-lock.json node_modules
+
 npm i @planit/design-system
 
 cp node_modules/@planit/design-system ./
 rm -rf ./bin
+
+echo 'Installation Complete.'
