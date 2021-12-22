dece93aecbf7c19dff133b84e44fd3c5c7c289e2
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-12-22 13:09:36 -0500

    chore(release): create temporary package.json

--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,7 @@ rm -rf package.json package-lock.json node_modules
 npm i @planit/design-system
 
 cd node_modules/@planit/design-system/bin
+echo '!!!!!!!!!!!!!!!'
 node edit-package-json.js $0 $1
 cd ../../../..
 cp -R node_modules/@planit/design-system/. ./
