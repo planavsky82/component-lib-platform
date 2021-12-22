c5365e47980cab9c9b087813139eccae5b770d66
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2021-12-22 13:17:01 -0500

    chore(release): create temporary package.json

--- bin/edit-package-json.js
+++ bin/edit-package-json.js
@@ -5,6 +5,6 @@ let fs = require('fs');
 const packageJson = JSON.parse(fs.readFileSync('../package-tmp.json', { encoding:'utf8' }));
 let args = process.argv;
 
-packageJson.name = args[2]
-packageJson.author = args[3] || ''
+packageJson.name = args[2] || 'my-design-system';
+packageJson.author = args[3] || '';
 fs.writeFileSync('../package-tmp.json', JSON.stringify(packageJson, null, 2));
