a78acadf6469afe5ee41461e427fa568a95f4227
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2022-05-01 22:47:50 -0400

    chore(release): create temporary package.json

--- scripts/edit-components-package-json.js
+++ scripts/edit-components-package-json.js
@@ -2,9 +2,13 @@
 
 let fs = require('fs');
 let args = process.argv.slice(2);
+let rootPath = '../../package.json';
+if (args[0] === 'react-components') {
+  rootPath = '../package.json';
+}
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', { encoding:'utf8' }));
-const rootPackageJson = JSON.parse(fs.readFileSync('../package.json', { encoding:'utf8' }));
+const rootPackageJson = JSON.parse(fs.readFileSync('', { encoding:'utf8' }));
 packageJson.name = '@planit/' + args[0];
 
 if (args[0] === 'react-components') {
