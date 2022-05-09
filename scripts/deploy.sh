31c447202af410694447d377b81ac6e53658d515
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.lan>
2022-05-09 00:45:27 -0400

    chore(release): create temporary package.json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ echo 'Build and Publishing @planit/components and @planit/react-components NPM P
 pushd stencil-web-components
 npm run build
 node ../scripts/edit-components-package-json.js components
-popd
+npm publish
 popd
 
 pushd stencil-react-wrappers
