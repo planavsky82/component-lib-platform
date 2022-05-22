b4021fef25c49a45f3ce7989d8a79a3c725bec40
Anthony Planavsky <anthonyplanavsky@Anthonys-MBP.home>
2022-05-22 18:30:05 -0400

    chore(release): create temporary package.json

--- stencil-web-components/src/components/planit-button/planit-button.stories.js
+++ stencil-web-components/src/components/planit-button/planit-button.stories.js
@@ -10,7 +10,7 @@ export const Button = () => {
     <h1 class="text-3xl mb-4">Button</h1>
     <section class="flex flex-col md:flex-row justify-between">
       <planit-button class="mb-3">Secondary Button</planit-button>
-      <planit-button class="mb-3">Primary Button</planit-button>
+      <planit-button class="mb-3" type="primary">Primary Button</planit-button>
       <planit-button class="mb-3">Button with Icon</planit-button>
       <planit-button class="mb-3" disabled="true">Disabled Button</planit-button>
     </section>
